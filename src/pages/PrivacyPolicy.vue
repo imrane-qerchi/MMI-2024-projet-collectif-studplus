552455870245b5c435fbb12fda2014033e784ad1
Moretti Tony <tony.moretti@edu.univ-fcomte.fr>
2024-06-13 08:42:43 +0200

    Page politique de confidentialité

--- src/pages/PrivacyPolicy.vue
+++ src/pages/PrivacyPolicy.vue
@@ -3,21 +3,21 @@
 </script>
 
 <template>
-    <section>
+    <section class="m-10">
         <div class="relative flex justify-between items-center w-full">
           <h1 class="text-[34px] border-b-2 pb-3 border-[#694C9B]">
             Politique de confidentialité
           </h1>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 I. Préambule
             </h2>
             <div>
-                <p>
+                <p class="pb-5">
                     La présente politique de confidentialité a pour but d’informer les utilisateurs du site :
                 </p>
-                <ul>
+                <ul class="pb-5 list-disc">
                     <li>
                         Sur la manière dont sont collectées leurs données personnelles. Sont considérées comme des données personnelles, toute information permettant d’identifier un utilisateur. A ce titre, il peut s’agir : de ses noms et prénoms, de son âge, de son adresse postale ou email, de sa localisation ou encore de son adresse IP (liste non-exhaustive) ;
                     </li>
@@ -39,15 +39,15 @@
                 </p>
             </div>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 II. Principes relatifs à la collecte et au traitement des données personnelles
             </h2>
             <div>
-                <p>
+                <p class="pb-5">
                     Conformément à l’article 5 du Règlement européen 2016/679, les données à caractère personnel sont :                
                 </p>
-                <ul>
+                <ul class="pb-5 list-disc">
                     <li>
                         Traitées de manière licite, loyale et transparente au regard de la personne concernée ;                    
                     </li>
@@ -67,10 +67,10 @@
                         Traitées de façon à garantir une sécurité appropriée des données collectées, y compris la protection contre le traitement non autorisé ou illicite et contre la perte, la destruction ou les dégâts d'origine accidentelle, à l'aide de mesures techniques ou organisationnelles appropriées.
                     </li>
                 </ul>
-                <p>
+                <p class="pb-5">
                     Le traitement n'est licite que si, et dans la mesure où, au moins une des conditions suivantes est remplie :                
                 </p>
-                <ul>
+                <ul class="pb-5 list-disc">
                     <li>
                         La personne concernée a consenti au traitement de ses données à caractère personnel pour une ou plusieurs finalités spécifiques ;                    
                     </li>
@@ -92,15 +92,15 @@
                 </ul>
             </div>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 III. Données à caractère personnel collectées et traitées dans le cadre de la navigation sur le site
             </h2>
             <div>
-                <p>
+                <p class="pb-5">
                     La présente politique de confidentialité a pour but d’informer les utilisateurs du site :                
                 </p>
-                <ul>
+                <ul class="pb-5 list-disc">
                     <li>
                         Sur la manière dont sont collectées leurs données personnelles. Sont considérées comme des données personnelles, toute information permettant d’identifier un utilisateur. A ce titre, il peut s’agir : de ses noms et prénoms, de son âge, de son adresse postale ou email, de sa localisation ou encore de son adresse IP (liste non-exhaustive) ;                    
                     </li>
@@ -122,14 +122,14 @@
                 </p>
             </div>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 Article 3.1 - Données collectées            
             </h2>
             <div>
-                <p>
+                <p class="pb-5">
                     Les données personnelles collectées dans le cadre de notre activité sont les suivantes :                </p>
-                <ul>
+                <ul class="pb-5 list-disc">
                     <li>
                         Prénom ;                    
                     </li>
@@ -149,10 +149,10 @@
                         Genre.
                     </li>
                 </ul>
-                <p>
+                <p class="pb-5">
                     La collecte et le traitement de ces données répond à la (aux) finalité(s) suivante(s) :                
                 </p>
-                <ul>
+                <ul class="pb-5 list-disc">
                     <li>
                         Gestion de l’espace client ;                    
                     </li>
@@ -168,28 +168,28 @@
                 </ul>
             </div>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 Article 3.2 - Mode de collecte des données
             </h2>
-            <p>
+            <p class="pb-5">
                 Lorsque vous utilisez notre site, sont automatiquement collectées les données suivantes :
             </p>
-            <p>
+            <p class="pb-5">
                 D’autres données personnelles sont collectées lorsque vous effectuez les opérations suivantes sur la plateforme :
             </p>
-            <p>
+            <p class="pb-5">
                 Elles sont conservées par le responsable du traitement dans des conditions raisonnables de sécurité, pour une durée de 3 ans.
             </p>
-            <p>
+            <p class="pb-5">
                 La société est susceptible de conserver certaines données à caractère personnel au-delà des délais annoncés ci-dessus afin de remplir ses obligations légales ou réglementaires.
             </p>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 Article 3.3 - Hébergement des données
             </h2>
-            <p>
+            <p class="pb-5">
                 Le site Stud+ est hébergé par : <br>
                 Stud+ <br>
                 2 Rue Apagnan, 25200 Montbéliard <br>
@@ -197,20 +197,20 @@
                 XXXXXXXXXX
             </p>
         </div>
-        <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
             IV. Responsable du traitement des données et délégué à la protection des données
-        </h2>
-        <div>
-            <h2>
+            </h2>
+            <h2 class="mb-5 mt-10">
                 Article 4.1 - Le responsable du traitement des données
             </h2>
-            <p>
+            <p class="pb-5">
                 Les données à caractère personnelles sont collectées par Stud+, SARL au capital de 6000€, dont le numéro d’immatriculation est le XXXXXXXXX.
             </p>
-            <p>
+            <p class="pb-5">
                 Le responsable du traitement des données à caractère personnel peut être contacté de la manière suivante :
             </p>
-            <p>
+            <p class="pb-5">
                 Par courrier à l’adresse : <br>
                 2 Rue Apagnan, 25200 Montbéliard <br>
                 Par email à l’adresse : <br>
@@ -219,25 +219,25 @@
                 XXXXXXXXXX
             </p>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 Article 4.2 - Le délégué à la protection des données
             </h2>
-            <p>
+            <p class="pb-5">
                 Le délégué à la protection des données de l’entreprise ou du responsable est à définir.
             </p>
-            <p>
+            <p class="pb-5">
                 Si vous estimez, après nous avoir contactés, que vos droits “Informatique et Libertés”, ne sont pas respectés, vous pouvez adresser une information à la CNIL.
             </p>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 V. Les droits de l’utilisateur en matière de collecte et de traitement des données
             </h2>
-            <p>
+            <p class="pb-5">
                 Tout utilisateur concerné par le traitement de ses données personnelles peut se prévaloir des droits suivants, en application du règlement européen 2016/679 et de la Loi Informatique et Liberté (Loi 78-17 du 6 janvier 1978) :
             </p>
-            <ul>
+            <ul class="pb-5 list-disc">
                 <li>
                     Droit d’accès, de rectification et droit à l’effacement des données (posés respectivement aux articles 15, 16 et 17 du RGPD) ;
                 </li>
@@ -257,28 +257,28 @@
                     Droit de saisir l’autorité de contrôle compétente (article 77 du RGPD). 
                 </li>
             </ul>
-            <p>
+            <p class="pb-5">
                 Pour exercer vos droits, veuillez adresser votre courrier à Stud+ au 2 Rue Apagnan, 25200 Montbléiard ou par mail à contact@studplus.fr.
             </p>
-            <p>
+            <p class="pb-5">
                 Afin que le responsable du traitement des données puisse faire droit à sa demande, l’utilisateur peut être tenu de lui communiquer certaines informations telles que : ses noms et prénoms, son adresse e-mail ainsi que son numéro de compte, d’espace personnel ou d’abonné. 
                 Consultez le site cnil.fr pour plus d’informations sur vos droits.
             </p>
         </div>
-        <div>
-            <h2>
+        <div class="m-5">
+            <h2 class="mb-5 mt-10">
                 VI. Conditions de modification de la politique de confidentialité
             </h2>
-            <p>
+            <p class="pb-5">
                 L’éditeur du site Stud+ se réserve le droit de pouvoir modifier la présente Politique à tout moment afin d’assurer aux utilisateurs du site sa conformité avec le droit en vigueur.
             </p>
-            <p>
+            <p class="pb-5">
                 Les éventuelles modifications ne sauraient avoir d’incidence sur les achats antérieurement effectués sur le site, lesquels restent soumis à la Politique en vigueur au moment de l’achat et telle qu’acceptée par l’utilisateur lors de la validation de l’achat.
             </p>
-            <p>
+            <p class="pb-5">
                 L’utilisateur est invité à prendre connaissance de cette Politique à chaque fois qu’il utilise nos services, sans qu’il soit nécessaire de l’en prévenir formellement. 
             </p>
-            <p>
+            <p class="pb-5">
                 La présente politique a été éditée le 12/03/2024.
             </p>
         </div>
