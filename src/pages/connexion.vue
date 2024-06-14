667e8f9aabb33bbe41b343300df290bdcc3ffa7e
Moretti Tony <tony.moretti@edu.univ-fcomte.fr>
2024-06-14 02:46:56 +0200

    Modifications connexion et inscription

--- src/pages/connexion.vue
+++ src/pages/connexion.vue
@@ -72,13 +72,15 @@ function checkError(error: any) {
     <div class="max-w-md w-full space-y-8">
       <div>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Connectez-vous à votre compte
+          Bon retour parmi nous !
         </h2>
+        <p class="text-center pt-5">Connectez vous à votre compte.</p>
       </div>
-      <form class="mt-8 space-y-6" @submit.prevent="doLogin">
+      <form class="mt-8 space-y-6 drop-shadow-lg" @submit.prevent="doLogin">
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
+            <p>Adresse e-mail</p>
             <label for="email-address" class="sr-only">Adresse e-mail</label>
             <input
               id="email-address"
@@ -86,11 +88,12 @@ function checkError(error: any) {
               type="email"
               v-model="email"
               required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none rounded-lg relative block w-full px-3 py-3 mb-5 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Adresse e-mail"
             />
           </div>
           <div>
+            <p>Mot de passe</p>
             <label for="password" class="sr-only">Mot de passe</label>
             <input
               id="password"
@@ -98,7 +101,7 @@ function checkError(error: any) {
               type="password"
               v-model="password"
               required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none rounded-lg relative block w-full px-3 py-3 mb-5 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Mot de passe"
             />
           </div>
@@ -107,7 +110,7 @@ function checkError(error: any) {
         <div>
           <button
             type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#694C9B] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            class="group relative w-full drop-shadow-lg flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-[#694C9B] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Se connecter
           </button>
--- src/pages/inscription.vue
+++ src/pages/inscription.vue
@@ -2,52 +2,72 @@
   <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
       <div>
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Créez votre compte</h2>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Bienvenue sur Stud+
+        </h2>
+        <p class="text-center pt-5">
+          Inscris toi ou connecte toi pour trouver des avantages
+        </p>
       </div>
-      <form class="mt-8 space-y-6" @submit.prevent="doRegister">
+      <form class="mt-8 space-y-6 drop-shadow-lg" @submit.prevent="doRegister">
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
+            <p>Nom</p>
             <label class="sr-only" for="">Nom</label>
             <input
               v-model="user_form.name"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none rounded-lg relative block w-full px-3 py-3 mb-5 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               type="text"
               placeholder="Nom"
               required
             />
           </div>
           <div>
-            <label class="sr-only" for="">Surnom</label>
+            <p>Pseudo</p>
+            <label class="sr-only" for="">Pseudo</label>
             <input
               v-model="user_form.username"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none rounded-lg relative block w-full px-3 py-3 mb-5 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               type="text"
-              placeholder="Surnom"
+              placeholder="Pseudo"
               required
             />
           </div>
           <div>
+            <p>Adresse e-mail</p>
             <label class="sr-only" for="">Adresse e-mail</label>
             <input
               v-model="user_form.email"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none rounded-lg relative block w-full px-3 py-3 mb-5 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               type="email"
               placeholder="Adresse e-mail"
               required
             />
           </div>
           <div>
+            <p>Mot de passe</p>
             <label class="sr-only" for="">Mot de passe</label>
             <input
               v-model="user_form.password"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none rounded-t-lg relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               type="password"
               placeholder="Mot de passe"
               minlength="8"
               required
             />
-            <p>Votre mot de passe doit contenir au moins :</p>
+          </div>
+          <div>
+            <label class="sr-only" for="">Confirmer le mot de passe</label>
+            <input
+              v-model="user_form.passwordConfirm"
+              class="appearance-none rounded-b-lg relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              type="password"
+              minlength="8"
+              placeholder="Confirmer le mot de passe"
+              required
+            />
+            <p class="mt-3">Votre mot de passe doit contenir au moins :</p>
             <ul class="list-disc list-inside">
               <li>8 caractères minimum</li>
               <li>1 minuscule</li>
@@ -55,29 +75,18 @@
               <li>1 caractère spécial</li>
             </ul>
           </div>
-          <div>
-            <label class="sr-only" for="">Confirmer le mot de passe</label>
-            <input
-              v-model="user_form.passwordConfirm"
-              class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              type="password"
-              minlength="8"
-              placeholder="Confirmer le mot de passe"
-              required
-            />
-          </div>
         </div>
 
         <div>
           <button
             @click="register_user"
             type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-[#694C9B] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            class="group relative w-full drop-shadow-lg mb-1 flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-[#694C9B] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             S'inscrire
           </button>
           <p class="pt-5">
-            Retour à la page de
+            Vous avez déjà un compte ?
             <button class="underline font-bold" @click="router.push('/connexion')">
               Connexion
             </button>
